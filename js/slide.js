d389365698c7fdd6df95cb656ffb7efee494369b
SeongWooSeok <tjddntjr98@naver.com>
2023-07-07 20:58:08 +0900

    TEST

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,6 @@
-let pages = 0;//현재 인덱스 번호
-let positionValue = 0;//images 위치값
-const IMAGE_WIDTH = 250;//한번 이동 시 IMAGE_WIDTH만큼 이동한다.
+let pages = 0;
+let positionValue = 0;
+const IMAGE_WIDTH = 400;
 //DOM
 const backBtn = document.querySelector(".back")
 const nextBtn = document.querySelector(".next")
@@ -8,14 +8,13 @@ const images = document.querySelector(".images")
 
 function next() {
   if (pages< 2) {
-    backBtn.removeAttribute('disabled')//뒤로 이동해 더이상 disabled가 아니여서 속성을 삭제한다.
-    positionValue -= IMAGE_WIDTH;//IMAGE_WIDTH의 증감을 positionValue에 저장한다.
+    backBtn.removeAttribute('disabled')
+    positionValue -= IMAGE_WIDTH;
     images.style.transform = `translateX(${positionValue}px)`;
-		//x축으로 positionValue만큼의 px을 이동한다.
-    pages += 1; //다음 페이지로 이동해서 pages를 1증가 시킨다.
+    pages += 1;
   }
-  if (pages === 2) { //
-    nextBtn.setAttribute('disabled', 'true')//마지막 장일 때 next버튼이 disabled된다.
+  if (pages === 2) {
+    nextBtn.setAttribute('disabled', 'true')
   }
 }
 
@@ -24,16 +23,16 @@ function back() {
     nextBtn.removeAttribute('disabled')
     positionValue += IMAGE_WIDTH;
     images.style.transform = `translateX(${positionValue}px)`;
-    pages -= 1; //이전 페이지로 이동해서 pages를 1감소 시킨다.
+    pages -= 1;
   }
   if (pages === 0) {
-    backBtn.setAttribute('disabled', 'true')//마지막 장일 때 back버튼이 disabled된다.
+    backBtn.setAttribute('disabled', 'true')
   }
 }
 
-function init() {  //초기 화면 상태
-  backBtn.setAttribute('disabled', 'true'); //속성이 disabled가 된다.
-  backBtn.addEventListener("click", back); //클릭시 다음으로 이동한다.
-  nextBtn.addEventListener("click", next);//클릭시 이전으로 이동한다.
+function init() {
+  backBtn.setAttribute('disabled', 'true');
+  backBtn.addEventListener("click", back);
+  nextBtn.addEventListener("click", next);
 }
 init();
