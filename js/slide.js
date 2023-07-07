4d0af536542ea75a09ad635649759376ebfb8591
SeongWooSeok <tjddntjr98@naver.com>
2023-07-07 22:31:33 +0900

    test

--- js/slide.js
+++ js/slide.js
@@ -5,7 +5,7 @@ const slideCount = slideImg.length; // 슬라이드 개수
 const prev = document.querySelector('.prev'); //이전 버튼
 const next = document.querySelector('.next'); //다음 버튼
 const slideWidth = 400; //한개의 슬라이드 넓이
-const slideMargin = 150; //슬라이드간의 margin 값
+const slideMargin = 100; //슬라이드간의 margin 값
 
 //전체 슬라이드 컨테이너 넓이 설정
 slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
