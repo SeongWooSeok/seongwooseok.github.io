bf39a25f16e08d2f466ad73cc937f958e46f8622
SeongWooSeok <tjddntjr98@naver.com>
2023-07-07 20:30:43 +0900

    test

--- js/slide.js
+++ js/slide.js
@@ -1,23 +1,43 @@
-const slides = document.querySelector(".slides");
-const leftbtn = document.querySelector('.toleft');
-const rightbtn=document.querySelector('.toright');
-let slideNo = 0;
-rightbtn.addEventListener("click",function goSlide(){
-    // 이미지가 총 6개라면 아래 4를 6으로 수정해야 합니다.
-    if (slideNo<4){
-        slideNo += 1;
- 
-        //직접 슬라이드를 해보고, 사진이 오른쪽 왼쪽으로 조금씩 삐뚤어지는지 확인한 뒤
-        //100.7 부분의 크기를 늘이거나 줄이면 됩니다.
-        document.querySelector(`#slide${slideNo}`).style.transform=`translateX(${-100.7*slideNo}%)`;  
-        }
+// Function to initialize the slideshow
+function initSlideShow() {
+  const slides = document.querySelectorAll('#slideShow .slides li');
+  let currentSlide = 0;
+
+  // Function to show a slide
+  function showSlide(index) {
+    slides.forEach((slide) => {
+      slide.classList.remove('active');
+    });
+    slides[index].classList.add('active');
+  }
+
+  // Function to navigate to the previous slide
+  function prevSlide() {
+    currentSlide--;
+    if (currentSlide < 0) {
+      currentSlide = slides.length - 1;
     }
-);
-leftbtn.addEventListener("click",function goSlide(){
-    if(slideNo>0){
-        slideNo -= 1;
-        const whatwillchange=document.querySelector(`#slide${slideNo+1}`).style;
-        whatwillchange.transform=`translateX(0%)`;
-        }
+    showSlide(currentSlide);
+  }
+
+  // Function to navigate to the next slide
+  function nextSlide() {
+    currentSlide++;
+    if (currentSlide >= slides.length) {
+      currentSlide = 0;
     }
-);
+    showSlide(currentSlide);
+  }
+
+  // Add event listeners to the previous and next buttons
+  const prevButton = document.querySelector('#slideShow .controller .prev');
+  const nextButton = document.querySelector('#slideShow .controller .next');
+  prevButton.addEventListener('click', prevSlide);
+  nextButton.addEventListener('click', nextSlide);
+
+  // Show the first slide
+  showSlide(currentSlide);
+}
+
+// Initialize the slideshow when the page loads
+window.addEventListener('load', initSlideShow);
