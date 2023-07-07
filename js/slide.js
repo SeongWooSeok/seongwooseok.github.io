2cc89dfc9b432d68d445ec0bc879c7dd5bfa42d7
SeongWooSeok <tjddntjr98@naver.com>
2023-07-07 20:50:31 +0900

    TEST

--- js/slide.js
+++ js/slide.js
@@ -1,37 +1,8 @@
-let curPos = 0;
-let postion = 0;
-const IMAGE_WIDTH = 640;
-const prevBtn = document.querySelector(".prev")
-const nextBtn = document.querySelector(".next")
-const images = document.querySelector(".images")
- 
-function prev(){
-  if(curPos > 0){
-    nextBtn.removeAttribute("disabled")
-    postion += IMAGE_WIDTH;
-    images.style.transform = `translateX(${postion}px)`;
-    curPos = curPos - 1;
-  }
-  if(curPos == 0){
-    prevBtn.setAttribute('disabled', 'true')
-  }
-}
-function next(){
-  if(curPos < 3){
-    prevBtn.removeAttribute("disabled")
-    postion -= IMAGE_WIDTH;
-    images.style.transform = `translateX(${postion}px)`;
-    curPos = curPos + 1;
-  }
-  if(curPos == 3){
-    nextBtn.setAttribute('disabled', 'true')
-  }
-}
- 
-function init(){
-  prevBtn.setAttribute('disabled', 'true')
-  prevBtn.addEventListener("click", prev)
-  nextBtn.addEventListener("click", next)
-}
- 
-init();
+$('#slide3>ul>li').hide();
+$('#slide3>ul>li:first-child').show();
+
+setInterval(function(){
+    $('#slide3>ul>li:first-child').fadeOut()
+    .next().fadeIn().end(1000)
+    .appendTo('#slide3>ul');
+},2000);
