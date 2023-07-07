68373fb58a4ec08377f9a98d09f5f166aa61695d
SeongWooSeok <tjddntjr98@naver.com>
2023-07-07 20:23:13 +0900

    test

--- js/slide.js
+++ js/slide.js
@@ -1,26 +1,23 @@
-const slides = document.querySelector('.slides'); //전체 슬라이드 컨테이너
-const slideImg = document.querySelectorAll('.slides li'); //모든 슬라이드들
-let currentIdx = 0; //현재 슬라이드 index
-const slideCount = slideImg.length; // 슬라이드 개수
-const prev = document.querySelector('.prev'); //이전 버튼
-const next = document.querySelector('.next'); //다음 버튼
-const slideWidth = 600; //한개의 슬라이드 넓이
-const slideMargin = 100; //슬라이드간의 margin 값
-
-//전체 슬라이드 컨테이너 넓이 설정
-slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
-
-function moveSlide(num) {
-  slides.style.left = -num * 600 + 'px';
-  currentIdx = num;
-}
-
-prev.addEventListener('click', function () {
-  if (currentIdx !== 0) moveSlide(currentIdx - 1);
-});
-
-next.addEventListener('click', function () {
-  if (currentIdx !== slideCount - 1) {
-    moveSlide(currentIdx + 1);
-  }
-});
+const slides = document.querySelector(".slides");
+const leftbtn = document.querySelector('.toleft');
+const rightbtn=document.querySelector('.toright');
+let slideNo = 0;
+rightbtn.addEventListener("click",function goSlide(){
+    // 이미지가 총 6개라면 아래 4를 6으로 수정해야 합니다.
+    if (slideNo<4){
+        slideNo += 1;
+ 
+        //직접 슬라이드를 해보고, 사진이 오른쪽 왼쪽으로 조금씩 삐뚤어지는지 확인한 뒤
+        //100.7 부분의 크기를 늘이거나 줄이면 됩니다.
+        document.querySelector(`#slide${slideNo}`).style.transform=`translateX(${-100.7*slideNo}%)`;  
+        }
+    }
+);
+leftbtn.addEventListener("click",function goSlide(){
+    if(slideNo>0){
+        slideNo -= 1;
+        const whatwillchange=document.querySelector(`#slide${slideNo+1}`).style;
+        whatwillchange.transform=`translateX(0%)`;
+        }
+    }
+);
